716073725a40caff86febe98c2fe227f8622bf08
qubiak <kubiak1986@gmail.com>
2020-12-01 12:48:47 +0100

    commit

--- src/main/java/src/RestClient.java
+++ src/main/java/src/RestClient.java
@@ -1,6 +1,8 @@
 package src;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,11 +12,19 @@ public class RestClient {
 
     public static void main(String[] args) throws IOException {
 
-        URL url = new URL("https://cat-fact.herokuapp.com/facts/random");
-        InputStreamReader reader = new InputStreamReader(url.openStream());
+        URL urlCatFakt = new URL("https://cat-fact.herokuapp.com/facts/random");
+        InputStreamReader readerCatFakt = new InputStreamReader(urlCatFakt.openStream());
 
-        CatFakt catFakt = new Gson().fromJson(reader, CatFakt.class);
+        CatFakt catFakt = new Gson().fromJson(readerCatFakt, CatFakt.class);
 
         System.out.println(catFakt.getText());
+
+        //If the answer is one-line and it is not worth implementing the model
+
+        URL urlCatImage = new URL("https://aws.random.cat/meow");
+        InputStreamReader readerCatImage = new InputStreamReader(urlCatImage.openStream());
+        JsonObject jsonObject =  JsonParser.parseReader(readerCatImage).getAsJsonObject();
+        String object =  jsonObject.get("file").getAsString();
+        System.out.println(object);
     }
 }
